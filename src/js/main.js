b8872ae092e7ae828cbbda3e5ddb759131e6f6e1
Don Henton <donhenton@gmail.com>
2016-08-03 13:10:29 -0500

    working pie demo 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
 //import $ from 'jquery';
-import HorizontalDemo from './demos/horizontalDemo'
+import HorizontalDemo from './demos/horizontalDemo';
+import PieChartDemo from './demos/pieChartDemo'
 var hDemo = null;
+var pDemo = null;
 
 
 $(document).ready(
     function () {
-        hDemo = new HorizontalDemo();
-        var update = hDemo.updateData.bind(hDemo);
-        $('#updateHorizontalData').on('click',update);
+        //hDemo = new HorizontalDemo();
+        //var update = hDemo.updateData.bind(hDemo);
+        //$('#updateHorizontalData').on('click',update);
+        pDemo = new PieChartDemo();
          
     });
