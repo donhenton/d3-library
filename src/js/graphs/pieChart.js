4332ed1a29018f5982ad6bac0e2810f78efd3a4b
Don Henton <donhenton@gmail.com>
2016-08-03 13:38:27 -0500

    adding legend box

--- src/js/graphs/pieChart.js
+++ src/js/graphs/pieChart.js
@@ -1,109 +1,113 @@
 import * as d3 from 'd3';
-import * as d3_scale from 'd3-scale';
+        import * as d3_scale from 'd3-scale';
+        export default class PieChart {
 
-
-export default class PieChart {
-
-    constructor(el, props, id, data)
-    {
+        constructor(el, props, id, data)
+        {
 
         this.svg = null;
-        this.element = el;
-        this.props = props;
-        this.context = id;
-        this.legendHeight = 26;
-        this.intervals = [];
-
-        this.pieWidth = this.props.width;
-        this.pieHeight = this.props.height;
-        this.radius = Math.min(this.pieWidth, this.pieHeight) / 2;
-        this.radius = 0.85 * this.radius;
-
-
-        this.svg = d3.select(this.element).append('svg')
+                this.element = el;
+                this.props = props;
+                this.context = id;
+                this.legendHeight = 26;
+                this.intervals = [];
+                this.pieWidth = this.props.width;
+                this.pieHeight = this.props.height;
+                this.radius = Math.min(this.pieWidth, this.pieHeight) / 2;
+                this.radius = 0.85 * this.radius;
+                this.svg = d3.select(this.element).append('svg')
                 .attr('id', id)
                 .attr('width', this.props.width)
                 .attr('height', this.props.height);
+                this.keyfunction = function (d) {
+                return d.color
+                };
+                this.build(data)
+        }
 
-
-        this.keyfunction = function (d) {
-            return d.color
-        };
-
-        this.build(data)
-    }
-
-    build(data)
-    {
+        build(data)
+        {
         this.arc = d3.arc()
                 .outerRadius(this.radius - 5)
                 .innerRadius(0);
-
-        this.labelArc = d3.arc()
-                .outerRadius(this.radius +2)
-                .innerRadius(this.radius +2);
-
-
-        let self = this;
-        //set pie function
-        this.pie = d3.pie()
+                this.labelArc = d3.arc()
+                .outerRadius(this.radius + 2)
+                .innerRadius(this.radius + 2);
+                let self = this;
+                //set pie function
+                this.pie = d3.pie()
                 .value(function (d) {
-                    return d.percentage;
+                return d.percentage;
                 })
                 .sort(function (d1, d2) {
-                    return d1.percentage > d2.percentage
+                return d1.percentage > d2.percentage
                 });
-
-        let arcs = this.pie(data);
-        let mainEntry = this.svg.append("g")
+                let arcs = this.pie(data);
+                let mainEntry = this.svg.append("g")
                 .attr("transform", "translate(" + this.radius + "," + this.props.height / 2 + ")");
-        this.groupsEnter = mainEntry
+                this.groupsEnter = mainEntry
                 .selectAll('.arc')
                 .data(arcs).enter().append('g').attr("class", "arc");
-
-        this.groupsEnter.append("path")
+                this.groupsEnter.append("path")
                 .attr("d", self.arc)
                 .attr("fill", function (d) {
-                    return d.data.color
+                return d.data.color
                 })
 
 
-        //////////////////////////// inner labels ////////////////////////////////////////
+                //////////////////////////// inner labels ////////////////////////////////////////
 
 
-        this.groupsEnter.append("text")
+                this.groupsEnter.append("text")
                 .attr("transform", function (d) {
-                    return "translate(" + self.labelArc.centroid(d) + ")";
+                return "translate(" + self.labelArc.centroid(d) + ")";
                 })
-               // .attr("dy", ".35em")
-               .style('font-family', "'graphikRegular', Helvetica, Arial, sans-serif")
+                // .attr("dy", ".35em")
+                .style('font-family', "'graphikRegular', Helvetica, Arial, sans-serif")
                 .style('fill', "#4d4e54")
                 .style('font-size', '12px')
                 .text(function (d) {
-                    return d.data.name;
+                return d.data.name;
                 });
+                ////////////////////////////  legend  ////////////////////////////////////////////
 
-        ////////////////////////////  legend  ////////////////////////////////////////////
+                let legendContainer = mainEntry.append("g").attr("class", 'legend-container')
 
-        let legendContainer = mainEntry.append("g").attr("class", 'legend-container');
-        legendContainer.attr('transform', "translate(" + (parseInt(this.props.width) - 200) + ",-40)")
-        self.legendGroups = legendContainer
+                .attr('fill', 'none')
+                .attr('stroke', '#646464')
+                .attr('stroke-width', '1px')
+                
+                .attr('stroke-linejoin', 'round')
+                 
+                 var borderPath = mainEntry.append("rect")
+       			.attr("x", 0)
+       			.attr("y", 0)
+       			.attr("height", 130)
+       			.attr("width", 170)
+       			.style("stroke", 'black')
+       			.style("fill", "none")
+       			.style("stroke-width", 2);
+                borderPath.attr('transform', "translate(" + (parseInt(this.props.width) - 310) + ",-45)")
+
+
+        legendContainer.attr('transform', "translate(" + (parseInt(this.props.width) - 300) + ",-40)")
+                self.legendGroups = legendContainer
                 .selectAll('g.legend-content')
                 .data(data.sort(function (d1, d2) {
-                    return d1.percentage < d2.percentage
+                return d1.percentage < d2.percentage
                 }))
-        //enter statement for legend items
-        self.legendGroupsEnter = self.legendGroups
+                //enter statement for legend items
+                self.legendGroupsEnter = self.legendGroups
                 .enter()
                 .append('g')
                 .attr('class', 'legend-content')
+                .attr('stroke','none')
                 .attr(
                         'transform', function (d, i) {
-                            return "translate(0, " + (10 + (i * self.legendHeight)) + ")";
-
+                        return "translate(0, " + (10 + (i * self.legendHeight)) + ")";
                         })
 
-        self.legendGroupsEnter.append('circle')
+                self.legendGroupsEnter.append('circle')
                 .attr('class', 'legend-circle')
                 .attr("r", 4)
 //                .attr( 
@@ -112,18 +116,16 @@ export default class PieChart {
 //                     
 //                })
                 .attr('fill', function (d, i) {
-                    return d.color;
+                return d.color;
                 });
-
-        self.legendGroupsEnter
+                self.legendGroupsEnter
 
                 .append("text")
                 .attr('class', 'percent')
 
                 .attr(
                         'transform', function (d, i) {
-                            return "translate(31, 0)";
-
+                        return "translate(31, 0)";
                         })
 
                 .style('font-family', "'graphikRegular', Helvetica, Arial, sans-serif")
@@ -132,12 +134,9 @@ export default class PieChart {
 
 
                 .text(function (d) {
-                    return d.percentage + '% -- ' + d.name;
+                return d.percentage + '% -- ' + d.name;
                 });
-
-
-
-    }
+        }
 
 }
 
